48d703e8d61c1403e2ec226a775495dc1e21803e
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-04-20 15:04:45 +0900

    feat: 팀 섞기 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [teams, setTeams] = useState(['가브리엘', '고니', '노아', '룩소', '세인', '센트', '슬링키', '아커', '유스', '참새', '해온', '황팽']);
+  const shuffleTeams = () => {
+    setTeams([...teams].sort(() => Math.random() - 0.5));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <h1>{teams.join(', ')}</h1>
+      </div>
+      <div>
+        <button onClick={() => shuffleTeams()}>팀 섞기</button>
+      </div>
+
     </div>
   );
 }
